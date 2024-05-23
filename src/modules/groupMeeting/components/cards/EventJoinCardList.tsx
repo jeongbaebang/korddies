9193c6e924a5c1406d62a6f97f6d45972788d781
jeongbaebang <jeongbaebang_dev@naver.com>
2024-05-23 15:35:40 +0900

    add: EventJoinCardList

---
+++ src/modules/groupMeeting/components/cards/EventJoinCardList.tsx
@@ -0,0 +1,27 @@
+import { ScrollView } from 'react-native';
+import React from 'react';
+import EventJoinCard, { EventJoinCardProps } from './EventJoinCard';
+
+type Props = {
+  data: EventJoinCardProps[];
+};
+
+const EventJoinCardList: React.FC<Props> = (props) => {
+  return (
+    <ScrollView>
+      {props.data.map((item, index) => {
+        return (
+          <EventJoinCard
+            key={index}
+            leftContent={item.leftContent}
+            rightContent={item.rightContent}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+export default EventJoinCardList;
+
+// const styles = StyleSheet.create({});
