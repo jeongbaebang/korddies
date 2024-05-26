b001c63a1fc90edb1b6133d31d7d3fc1a123d5fc
jeongbaebang <jeongbaebang_dev@naver.com>
2024-05-26 13:22:04 +0900

    update: add implements type

--- src/shared/utils/cache.ts
+++ src/shared/utils/cache.ts
@@ -1,17 +1,24 @@
 const ERROR_MESSAGE =
   'Invalid cache key: The requested key is not present in the cache.';
 
+interface Cache<K, V> {
+  find(key: K): V;
+  set(key: K, value: V): void;
+  has(key: K): boolean;
+  calculate(key: K, fn: (key: K) => V): V;
+}
+
 /**
  * @see https://github.com/jeongbaebang/korddies/wiki/Docs#cache
  */
-class Cache<K, V> {
+class Cache<K, V> implements Cache<K, V> {
   private cache: Map<K, V>;
 
   constructor() {
     this.cache = new Map();
   }
 
-  find(key: K): V {
+  find(key: K) {
     const cachedValue = this.cache.get(key);
 
     if (cachedValue === undefined) {
@@ -21,15 +28,15 @@ class Cache<K, V> {
     return cachedValue;
   }
 
-  set(key: K, value: V): void {
+  set(key: K, value: V) {
     this.cache.set(key, value);
   }
 
-  has(key: K): boolean {
+  has(key: K) {
     return this.cache.has(key);
   }
 
-  calculate(key: K, fn: (key: K) => V): V {
+  calculate(key: K, fn: (key: K) => V) {
     if (this.has(key)) {
       return this.find(key);
     }
